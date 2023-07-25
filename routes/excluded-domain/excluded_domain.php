1809ba6e0c65297b399b73ad7b0dc2ea499fd02b
ashim-viserx <ashim.viserx@gmail.com>
2023-07-25 13:59:05 +0600

    #308 - route name change

--- routes/excluded-domain/excluded_domain.php
+++ routes/excluded-domain/excluded_domain.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ExcludedDomain\ExcludedDomainController;
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::apiResource('excluded-domain', ExcludedDomainController::class);
+    Route::apiResource('excluded-domains', ExcludedDomainController::class);
 });
