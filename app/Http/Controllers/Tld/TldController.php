abe2c4d10ebd58aba350c96f0d180b62be73549b
ashim-viserx <ashim.viserx@gmail.com>
2023-07-25 14:51:54 +0600

    #313 - TLD Module - setup

---
+++ app/Http/Controllers/Tld/TldController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Tld;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TldController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, string $campaign_id)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $campaign_id, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $campaign_id, string $id)
+    {
+        //
+    }
+}
