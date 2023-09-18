edb0e800b49a8f3b013d854712288fd79558d93f
ashim-viserx <ashim.viserx@gmail.com>
2023-09-18 18:41:42 +0600

    #463 get country and city visitor from old analytics table

--- routes/api.php
+++ routes/api.php
@@ -11,17 +11,6 @@
 |
 */
 
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/test', function () {
-    return DB::connection('choto_analytics_db')
-        ->table('analytics')
-        ->orderBy('id', 'asc')
-        ->take(10)
-        ->get();
-});
-
 // auth routes
 require __DIR__ . '/auth/auth.php';
 
