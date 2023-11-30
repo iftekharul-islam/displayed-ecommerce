ef86b4e5ca6ee4aa5c4a131453a9889f8835f1c5
iftekharul islam <iftekharulislam.cse@gmail.com>
2023-11-30 18:36:10 +0600

    hold page and redirect make delay

--- resources/views/hold_for_count.blade.php
+++ resources/views/hold_for_count.blade.php
@@ -52,15 +52,15 @@
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-N282D4BJ');
+        })(window,document,'script','dataLayer','GTM-WH8TXT85');
     </script>
     <!-- End Google Tag Manager -->
 </head>
 <body>
     <!-- Google Tag Manager (noscript) -->
     <noscript>
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N282D4BJ"
-                  height="0" width="0" style="display:none;visibility:hidden">
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WH8TXT85"
+                height="0" width="0" style="display:none;visibility:hidden">
         </iframe>
     </noscript>
     <!-- End Google Tag Manager (noscript) -->
