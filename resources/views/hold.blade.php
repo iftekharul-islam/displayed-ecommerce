6a2d5e37745f72b6dee879874a590569f59a5878
iftekharul islam <iftekharulislam.cse@gmail.com>
2023-11-30 18:03:29 +0600

    hold page and redirect make delay

--- resources/views/hold.blade.php
+++ resources/views/hold.blade.php
@@ -22,20 +22,18 @@
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-MWT7B3TH');
+        })(window,document,'script','dataLayer','GTM-TSQZ2H27');
     </script>
     <!-- End Google Tag Manager -->
 </head>
 <body>
 <!-- Google Tag Manager (noscript) -->
 <noscript>
-    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MWT7B3TH"
-                  height="0" width="0" style="display:none;visibility:hidden">
-
-    </iframe>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TSQZ2H27"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe>
 </noscript>
 <!-- End Google Tag Manager (noscript) -->
-<div id="message">Verifying you are a human......</div>
+<div id="message">Choto is verifying you are a human......</div>
 
 <script>
     document.addEventListener("DOMContentLoaded", function() {
